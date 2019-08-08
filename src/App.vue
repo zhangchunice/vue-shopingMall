553d60ebfebf619afe57d0fd2938d64358d0d564
安锦流年 <412829230@qq.com>
2019-08-08 15:58:50 +0800

    商城项目更改

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,51 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <!-- 顶部header区域 -->
+    <van-nav-bar
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    />
+
+    <!-- 中间的内容 路由router-view区域 -->
+    <h1>这是app组件</h1>
+
+    <!-- 底部 Tarbar区域 -->
+
+    <van-tabbar v-model="active">
+      <van-tabbar-item name="home" icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item name="search" icon="search">标签</van-tabbar-item>
+      <van-tabbar-item name="friends" icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item name="setting" icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import Vue from 'vue'
+import { Toast } from 'vant'
+Vue.use(Toast)
+
+export default {
+  data () {
+    return {
+      active: 'home'
+    }
+  },
+  methods: {
+    onClickLeft () {
+      Toast('返回')
+    },
+    onClickRight () {
+      Toast('按钮')
     }
   }
 }
+</script>
+
+<style lang="sass" scoped>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
+import router from './router.js'
+import { NavBar, Toast, Tabbar, TabbarItem } from 'vant'
+import 'vant/lib/index.css'
+Vue.use(NavBar).use(Toast).use(Tabbar).use(TabbarItem)
 
 new Vue({
   router,
-  store,
-  render: h => h(App)
+  render: c => c(App)
 }).$mount('#app')
